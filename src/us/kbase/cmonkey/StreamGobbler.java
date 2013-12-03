c88d8e0b8bcb91cda1f5aa3fe2a7809247204929
Alexey Kazakov <aekazakov@lbl.gov>
2013-12-02 16:26:09 -0800

    minor update

--- src/us/kbase/cmonkey/StreamGobbler.java
+++ src/us/kbase/cmonkey/StreamGobbler.java
@@ -1,7 +1,6 @@
 package us.kbase.cmonkey;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -37,41 +36,38 @@ class StreamGobbler extends Thread
 
     public void run()
     {
-        try
-        {
-        	InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            BufferedWriter writer = null;
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        FileWriter writer = null;
+       	try {
             if (logfileName != null) {
-            	writer = new BufferedWriter(new FileWriter(logfileName));
+					writer = new FileWriter(logfileName);
             };
             String line=null;
-            while ( (line = br.readLine()) != null) {
-            	System.out.println(type + ">" + line);
-            	if (logfileName != null) {
-            		writer.write(line);
-            		writer.newLine();
-            	}
-            	//Catch "INFO     Iteration # " pattern
-           		if (iterationPattern.matcher(line).find()){
-            		//call CmonkeyServerImpl
-           			String[] lineParts = iterationNumberPattern.split(line);
-           			System.out.println("ITERATION: " + lineParts[lineParts.length - 1]);
-               		if (jobId != null) {
-               			String status = "cMonkey iteration " + lineParts[lineParts.length - 1];
-               			System.out.println(status);
-            			CmonkeyServerImpl.updateJobProgress(jobId, status, token);
-            		}
-            	}
-            }
+			while ( (line = br.readLine()) != null) {
+				//System.out.println(type + ">" + line);
+				if (logfileName != null) {
+					writer.write(line);
+				}
+				//Catch "INFO     Iteration # " pattern
+				if (iterationPattern.matcher(line).find()){
+					//call CmonkeyServerImpl
+					String[] lineParts = iterationNumberPattern.split(line);
+					System.out.println("ITERATION: " + lineParts[lineParts.length - 1]);
+			   		if (jobId != null) {
+			   			String status = "cMonkey iteration " + lineParts[lineParts.length - 1];
+			   			System.out.println(status);
+						CmonkeyServerImpl.updateJobProgress(jobId, status, token);
+					}
+				}
+			};
        		if (logfileName != null) {
         		writer.write("STREAMGOBBLER RUN FINISHED");
-        		writer.newLine();
         		writer.close();
-       		}
-        } catch (IOException ioe)
-        {
-        	ioe.printStackTrace();  
-        }
+       		};
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
