5dbe8b988ba12ec76a450d019ac0b6e6094a032e
Alexey Kazakov <aekazakov@lbl.gov>
2013-12-12 13:32:37 -0800

    shell script update

--- scripts/run_cmonkey.sh
+++ scripts/run_cmonkey.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+mkdir -p $2
+cat >> /var/tmp/cmonkey.txt << EOF
+job $2 started
+EOF
 echo "$2 is output"
-java -jar $KB_TOP/lib/jars/cmonkey/cmonkey.jar $@
+java -jar $KB_TOP/lib/jars/cmonkey/cmonkey.jar $@ >> /var/tmp/cmonkey.txt
 tar cvfz $2.tgz $2
 rm -rf $2
+cat >> /var/tmp/cmonkey.txt << EOF
+job $2 finished
+EOF
